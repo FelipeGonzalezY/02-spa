ec686097490c2b2635b0e0e4305b2a7a6d92ed8b
FelipeGonzalezY <fgonzalezy@gmail.com>
2019-08-13 03:12:53 -0400

    buscador implementado

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -4,8 +4,7 @@ import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
-  templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+  templateUrl: './heroe.component.html'
 })
 export class HeroeComponent implements OnInit {
 
